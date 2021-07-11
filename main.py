0ea113513e1d1bd2f6dd6b4e7e0f4f5259767385
Kyle Guerrero <kyleguerrero85@gmail.com>
2021-07-10 22:43:31 -0500

    update: setup functions to be solved and used in unit testing

--- main.py
+++ main.py
@@ -45,28 +45,109 @@ def main():
     # Step 5: loop through the list's above, and add each object created to the **animals** list
     animals = []
 
-    # Step 6: Solve the requirements below....
-    # HINT: Be sure to create new lists when sorting
-    # and NOT altering a list in place. Assign the result to the **sorted_animals** list
+    # =============================================================================================================================
+    # READ FIRST!!!!
     #
-    # https://stackoverflow.com/questions/403421/how-to-sort-a-list-of-objects-based-on-an-attribute-of-the-objects
+    # Below are a series of functions that need to be implemeneted.
+    # Be sure you are NOT altering a list in place rather creating a new one
+    # and assigning your result to the **sorted_animals** list
+    # when solving each problem
+    #
+    # HINT: https://stackoverflow.com/questions/403421/how-to-sort-a-list-of-objects-based-on-an-attribute-of-the-objects
+    # =============================================================================================================================
 
-    sorted_animals = None
+    # Step 7: Goto the function orderByYearNamedDesc and follow the instructions
     print("=== List all the animals in descending order by year named ===")
+    orderByYearNamedDesc(animals)
 
+    # Step 8:
     print("\n\n=== List all the animals alphabetically ===")
+    sortAnimalsByName(animals)
 
+    # Step 9:
     print("\n\n=== List all the animals order by how they move ===")
+    sortAnimalsByMovement(animals)
 
+    # Step 10:
     print("\n\n=== List only those animals the breath with lungs ===")
+    findAnimalsWithLungs(animals)
 
+    # Step 11:
     print(
         "\n\n=== List only those animals that breath with lungs and were named in 1758 ==="
     )
+    findByLungsAndYear(animals)
 
+    # Step 12:
     print("\n\n=== List only those animals that lay eggs and breath with lungs ===")
+    findByEggsAndLungs(animals)
 
+    # Step 13:
     print("\n\n=== List alphabetically only those animals that were named in 1758 ===")
+    sortByNameAndYear(animals)
+
+
+def orderByYearNamedDesc(animalsList):
+    sorted_animals = None
+
+    # =====================================
+    # Leave this here for the test cases
+    return sorted_animals
+    # =====================================
+
+
+def sortAnimalsByName(animalsList):
+    sorted_animals = None
+
+    # =====================================
+    # Leave this here for the test cases
+    return sorted_animals
+    # =====================================
+
+
+def sortAnimalsByMovement(animlasList):
+    sorted_animals = None
+
+    # =====================================
+    # Leave this here for the test cases
+    return sorted_animals
+    # =====================================
+
+
+def findAnimalsWithLungs(animalsList):
+    sorted_animals = None
+
+    # =====================================
+    # Leave this here for the test cases
+    return sorted_animals
+    # =====================================
+
+
+def findByLungsAndYear(animalsList):
+    sorted_animals = None
+
+    # =====================================
+    # Leave this here for the test cases
+    return sorted_animals
+    # =====================================
+
+
+def findByEggsAndLungs(animalsList):
+    sorted_animals = None
+
+    # =====================================
+    # Leave this here for the test cases
+    return sorted_animals
+    # =====================================
+
+
+def sortByNameAndYear(animalsList):
+    sorted_animals = None
+
+    # =====================================
+    # Leave this here for the test cases
+    return sorted_animals
+    # =====================================
 
 
 if __name__ == "__main__":
